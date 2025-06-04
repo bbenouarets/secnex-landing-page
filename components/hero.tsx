8a417556b9ce902fef410041ecf12b36abb65fc5
Björn Benouarets <bbenouarets@proton.me>
2025-06-04 17:40:30 +0200

    Initalize project

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { AnimatedBlurBall } from "@/components/animation";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 import {
