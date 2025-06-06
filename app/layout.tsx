e511a6dc34ba2bd62644e8981a710171e29d3521
Björn Benouarets <bbenouarets@proton.me>
2025-06-06 02:16:30 +0200

    feat: Add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -61,6 +63,7 @@ export default function RootLayout({
 					data-do-not-track="true"
 				></script>
 			</head>
+			<SpeedInsights />
 		</html>
 	);
 }
